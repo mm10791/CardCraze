4d512bae08dfa5e71ac66387afa3fd8e2b2cd420
Max Matthews <madisonmatthews0202@gmail.com>
2025-03-30 12:03:35 -0400

    fix .csproj file

--- CardCraze/obj/Debug/net8.0/CardCraze.AssemblyInfo.cs
+++ CardCraze/obj/Debug/net8.0/CardCraze.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CardCraze")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+efa6049f9024bfa98f4b03f023385f486b2b3aa6")]
 [assembly: System.Reflection.AssemblyProductAttribute("CardCraze")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CardCraze")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
