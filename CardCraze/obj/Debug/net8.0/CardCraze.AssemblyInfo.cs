c6be0c92dc77c38716185585553a60457bd42d0b
Adrian Agius <adrian_joshua@msn.com>
2025-04-14 12:33:12 -0400

    fixed wishlist add to cart functionality

--- CardCraze/obj/Debug/net8.0/CardCraze.AssemblyInfo.cs
+++ CardCraze/obj/Debug/net8.0/CardCraze.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CardCraze")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a4a78ba9e73b9ab791f1fddf52ce263ab9f35f3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08612f78134ee4777e351b9a564c2b2849f14cde")]
 [assembly: System.Reflection.AssemblyProductAttribute("CardCraze")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CardCraze")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
