822394b2f8ec061a2fb751d4604cfb2b97252b59
Max Matthews <madisonmatthews0202@gmail.com>
2025-04-14 01:30:03 -0400

    Added UnitTesting for Cards and Browsing

--- CardCrazeAPI/obj/Debug/net8.0/CardCrazeAPI.AssemblyInfo.cs
+++ CardCrazeAPI/obj/Debug/net8.0/CardCrazeAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CardCrazeAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30bc742386b81368468543b84dd6f7feb5719bbe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a4a78ba9e73b9ab791f1fddf52ce263ab9f35f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("CardCrazeAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CardCrazeAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
